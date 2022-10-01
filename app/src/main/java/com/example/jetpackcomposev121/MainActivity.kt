7db97c876e94f5045d0f763abdce39b147f365ed
nameisjayant <jayntkumar99@gmail.com>
2022-10-01 23:12:57 +0530

    suggestion chips compose

--- app/src/main/java/com/example/jetpackcomposev121/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposev121/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.jetpackcomposev121.component.screen.SuggestionChipLayout
 import com.example.jetpackcomposev121.ui.theme.JetpackComposeV121Theme
 
 class MainActivity : ComponentActivity() {
@@ -19,10 +20,9 @@ class MainActivity : ComponentActivity() {
             JetpackComposeV121Theme {
                 // A surface container using the 'background' color from the theme
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    SuggestionChipLayout()
                 }
             }
         }
---
+++ app/src/main/java/com/example/jetpackcomposev121/component/screen/SuggestionChipLayout.kt
@@ -0,0 +1,67 @@
+package com.example.jetpackcomposev121.component.screen
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import com.example.jetpackcomposev121.ui.theme.Purple80
+import com.example.jetpackcomposev121.ui.theme.PurpleGrey40
+
+@Composable
+fun SuggestionChipLayout() {
+
+    val chipsData by remember { mutableStateOf(listOf("India", "France", "Spain")) }
+    var chipState by remember { mutableStateOf("") }
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 15.dp),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            chipsData.forEach {
+                SuggestionChipEachRow(label = it, selected = it == chipState) { chip ->
+                    chipState = chip
+                }
+            }
+        }
+    }
+
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun SuggestionChipEachRow(
+    label: String,
+    selected: Boolean,
+    onChipChange: (String) -> Unit
+) {
+
+    SuggestionChip(onClick = {
+        if (!selected)
+            onChipChange(label)
+        else
+            onChipChange("")
+    }, label = {
+        Text(text = label)
+    }, //modifier = Modifier.padding(horizontal = 10.dp),
+        shape = RoundedCornerShape(8.dp),
+        colors = SuggestionChipDefaults.suggestionChipColors(
+            containerColor = if (selected) Purple80 else Color.Transparent
+        ),
+        border = SuggestionChipDefaults.suggestionChipBorder(
+            borderWidth = 1.dp,
+            borderColor = if (selected) Color.Transparent else PurpleGrey40
+        )
+    )
+
+}
